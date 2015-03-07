655a4cd9297b4a00fd5f6d82bf8e915d54301468
yanpenn <845917370@qq.com>
2015-03-07 14:32:52 +0800

    add cat

--- source/intetnet/mv.sh
+++ source/intetnet/mv.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/bash
 
+cd ~/manpages/source/intetnet/ 
+mv *.ronn ~/manpages/ronn/
+mv *.?.html ~/manpages/html/
+mv *.1 ~/manpages/man/zh_CN/man1/
+mv *.5 ~/manpages/man/zh_CN/man5/
+echo "move done"
+
+ronn ~/manpages/html/list.ronn
+echo "update list done" 
+
+cd ~/manpages/ronn/
 mv *.ronn ~/manpages/ronn/
 mv *.?.html ~/manpages/html/
 mv *.1 ~/manpages/man/zh_CN/man1/
