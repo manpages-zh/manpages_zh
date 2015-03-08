393c4c8d9e4edef0bb45cdc2fe96a6e3dffc7187
yanpenn <845917370@qq.com>
2015-03-08 19:46:31 +0800

    add ps,modifiy mupdf,mv

--- source/intetnet/mv.sh
+++ source/intetnet/mv.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/bash
 
-cd ~/manpages/source/intetnet/ 
-mv *.ronn ~/manpages/ronn/
-mv *.?.html ~/manpages/html/
-mv *.1 ~/manpages/man/zh_CN/man1/
-mv *.5 ~/manpages/man/zh_CN/man5/
+for Path in ~/manpages/source/intetnet/ ~/manpages/ronn/; do
+cd $Path
+
+mv -u *.ronn ~/manpages/ronn/
+mv -u *.?.html ~/manpages/html/
+mv -u *.1 ~/manpages/man/zh_CN/man1/
+mv -u *.5 ~/manpages/man/zh_CN/man5/
 echo "move done"
 
 ronn ~/manpages/html/list.ronn
 echo "update list done" 
 
-cd ~/manpages/ronn/
-mv *.ronn ~/manpages/ronn/
-mv *.?.html ~/manpages/html/
-mv *.1 ~/manpages/man/zh_CN/man1/
-mv *.5 ~/manpages/man/zh_CN/man5/
-echo "move done"
-
-ronn ~/manpages/html/list.ronn
-echo "update list done" 
+done
