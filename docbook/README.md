edd045770276e90f1d5170ee9333e28e2a0f44f3
Boyuan Yang <073plan@gmail.com>
2015-04-11 22:12:05 +0800

    docbook: add docbook2man generate script

--- docbook/README.md
+++ docbook/README.md
@@ -7,3 +7,5 @@ https://github.com/mrry/ciel/blob/master/debian/manpage.xml.ex
 http://www.tldp.org/HOWTO/DocBook-Install/using.html
 
 http://www.docbook.org/tdg5/en/html/refentry.html
+
+http://zh.wikipedia.org/wiki/%E7%A9%BA%E6%A0%BC
--- docbook/generate.sh
+++ docbook/generate.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-# see http://docbook2x.sourceforge.net/screenshots/docbook2X/Converting-to-man-pages.html
+# Script used to convert DocBook XML file to groff-format man page.
+# author: @hosiet
+#
+# also see http://docbook2x.sourceforge.net/screenshots/docbook2X/Converting-to-man-pages.html
 
-db2x_xsltproc -s man test.xml -o test.mxml
-db2x_manxml test.mxml
+declare -a LIST
+LIST=(cd.1p)
+for NAME in ${LIST}; do
+    db2x_xsltproc -s man ${NAME}.xml -o ${NAME}.mxml
+    db2x_manxml --encoding="UTF-8" ${NAME}.mxml
+done
+rm ./*.mxml
